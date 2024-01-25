7de1d8a24f22a02f2cb6b01f10d3731ad2dc6e5a
MilenaDuarte22 <milena.duarte94@gmail.com>
2024-01-25 18:14:12 -0300

    user login
    starting the user login system

--- server.js
+++ server.js
@@ -13,6 +13,8 @@ const port = process.env.PORT || 5000;
 //parsing our data
 app.use(express.json());
 app.use("/api/contacts", require("./routes/contactRoutes"));
+//users - login
+app.use("/api/users", require("./routes/userRoute"));
 //using our error handler file
 app.use(errorHandler);
 
